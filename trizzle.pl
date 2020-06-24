93268d9c6bab40e93f134e449cb47cb807d83856
Giovanni <lorenzogiovanni1996@gmail.com>
2020-06-23 23:50:46 -0300

    Create trizzle.pl
    Creado:
    + prediado principal del juego desplazar/5 con sus 4 variantes.
    + moverCol
    + moverFil
    + rotarN
    
    Falta:
    - recorrerFil: recorre todas las filas del tablero para que despues se  colapse todas las mamushkas que pueda sobre el elemento Principal.
    - recorrerCol: idem anterior.
    - burbujearTablero: este tiene que hacer que todos los espacios vacios vayan para arriba onda que simulen las mamushkas caer por la gravedad.
    - rellenar: rellenan los espacios vacios con mamushkas.

---
+++ trizzle.pl
@@ -0,0 +1,67 @@
+:- use_rendering(table).
+
+% desplazar(+der, +Num, +Cant, +Tablero, -EvolTablero):
+% Predicado principal del juego, desplaza la fila o columna 
+% elegida en una cantidad de lugares hacia una direccion tambien elegida.
+desplazar(der, Num, Cant, Tablero, [Tablero1, Tablero2, Tablero3, Tablero4]) :-
+    moverFila(Num, Cant, Tablero, Tablero1),
+    append(Tablero1, [], Aux),
+    recorrerCol(Aux, Num, Tablero2),
+    burbujearTablero(Tablero2, Tablero3),
+    rellenar(Tablero3, Tablero4).
+
+desplazar(izq, Num, Cant, Tablero, [Tablero1, Tablero2, Tablero3, Tablero4]) :-
+    moverFila(Num, (-Cant), Tablero, Tablero1),
+    append(Tablero1, [], Aux),
+    recorrerCol(Aux, Num, Tablero2),
+    burbujearTablero(Tablero2, Tablero3),
+    rellenar(Tablero3, Tablero4).
+
+desplazar(arriba, Num, Cant, Tablero, [Tablero1, Tablero2, Tablero3, Tablero4]) :-
+    moverCol(Num, (-Cant), Tablero, Tablero1),
+    append(Tablero1, [], Aux),
+    recorrerFil(Aux, Num, Tablero2),
+    burbujearTablero(Tablero2, Tablero3),
+    rellenar(Tablero3, Tablero4).
+
+desplazar(abajo, Num, Cant, Tablero, [Tablero1, Tablero2, Tablero3, Tablero4]) :-
+    moverCol(Num, Cant, Tablero, Tablero1),
+    append(Tablero1, [], Aux),
+    recorrerFil(Aux, Num, Tablero2),
+    burbujearTablero(Tablero2, Tablero3),
+    rellenar(Tablero3, Tablero4).
+
+% desplazar_columna(+N, +Cant, +Tablero, -D):
+% Desplaza la N-esima columna del tablero en una cant de posiciones, pueden ser arriba o abajo.
+moverCol(N, Cant, Tablero, D) :-
+    transpose(Tablero, Trasp),
+    moverFila(N, Cant, Trasp, D1),
+    transpose(D1, D).
+
+% desplazar_columna(+N, +Cant, +Tablero, -D):
+% Desplaza la N-esima fila del tablero en una cantidad de posiciones, puede ser izquierda o derecha.
+moverFila(N, Cant, [T|Ts], [T|D]) :-
+    N > 0,
+    N1 is N-1,
+    moverFila(N1, Cant, Ts, D).
+moverFila(0, Cant, [T|Ts], D) :-
+    rotarN(Cant, T, D1),
+    append([D1], Ts, D).
+
+% rotarN(+N, +LIni, -LFin):
+% Desplaza una lista N posiciones a derecha o izquierda, segun el signo de N.
+rotarN(0, L, L).
+rotarN(N, L1, L2) :-
+    N < 0,
+    rotar("izq", L1, L),
+    N1 is N+1,
+    rotarN(N1, L, L2).
+rotarN(N, L1, L2) :-
+    N > 0,
+    rotar("der", L1, L),
+    N1 is N-1,
+    rotarN(N1, L, L2).
+
+rotar("der", L, [T|H]) :- append(H, [T], L).
+rotar("izq", [H|T], L) :- append(T, [H], L).
+
